fcface4e6f24f9d40a0723a2f152a749d6bb5b5f
Scott Richardson <srich1550@gmail.com>
2025-10-13 23:34:19 -0600

    added GameDao and MemoryGameDao

---
+++ server/src/main/java/dataaccess/MemoryGameDao.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDao implements GameDao {
+    HashMap<Integer, GameData> games = new HashMap<>();
+
+    //might need to figure out a way to make sure game doesn't exist yet
+    @Override
+    public void createGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameId) {
+        return games.get(gameId);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+}
