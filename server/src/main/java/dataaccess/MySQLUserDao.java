2e6a2021b502a06f15fde9c1660b644878e8aabe
Scott Richardson <srich1550@gmail.com>
2025-10-29 17:54:22 -0600

    implemented getUser

--- server/src/main/java/dataaccess/MySQLUserDao.java
+++ server/src/main/java/dataaccess/MySQLUserDao.java
@@ -2,6 +2,11 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQLUserDao implements UserDao {
     @Override
     public void createUser(UserData user) throws DataAccessException {
@@ -10,7 +15,22 @@ public class MySQLUserDao implements UserDao {
     }
 
     @Override
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to get user: " + e.getMessage(), e);
+        }
         return null;
     }
 
--- server/src/test/java/dataaccess/MySQLUserDaoTests.java
+++ server/src/test/java/dataaccess/MySQLUserDaoTests.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MySQLUserDaoTests {
     private MySQLUserDao userDao;
@@ -37,7 +36,36 @@ class MySQLUserDaoTests {
 
     }
 
+    @Test
+    @DisplayName("Successful user query")
+    void goodGetUser() throws DataAccessException {
+        UserData user = new UserData("usopp", "hashedPassword", "usopp@gmail.com");
+        userDao.createUser(user);
 
+        UserData result = assertDoesNotThrow(() -> userDao.getUser("usopp"));
+
+        assertNotNull(result);
+        assertEquals("usopp", result.username());
+        assertEquals("hashedPassword", result.password());
+        assertEquals("usopp@gmail.com", result.email());
+    }
+
+    @Test
+    @DisplayName("Search for invalid user")
+    void badGetUser() throws DataAccessException {
+        UserData result = userDao.getUser("theOnePiece");
+        assertNull(result);
+    }
+
+    @Test
+    @DisplayName("Successful clear")
+    void clearTest() throws DataAccessException {
+        UserData user = new UserData("chopper", "hashedPassword", "chopper@gmail.com");
+        userDao.createUser(user);
+
+        assertDoesNotThrow(() -> userDao.clear());
+
+    }
 
 
 
