096a00d7cd980ab04ed986414848c94bc45a11f6
Scott Richardson <srich1550@gmail.com>
2025-12-01 18:27:18 -0700

    connect and leave broadcast to the game now, needs more work

--- shared/src/main/java/websocket/WebSocketHandler.java
+++ shared/src/main/java/websocket/WebSocketHandler.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import io.javalin.websocket.*;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.commands.UserGameCommand;
+import websocket.messages.NotificationMessage;
+
+import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
@@ -41,18 +44,22 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         System.out.println("Websocket closed");
     }
 
-    private void handleConnectCommand(UserGameCommand command, Session session) {
+    private void handleConnectCommand(UserGameCommand command, Session session) throws IOException {
         System.out.println("handling a connect command");
         sessions.addSessionToGame(command.getGameID(), session);
+        var notification = new NotificationMessage("Player joined."); //TODO figure out how to send their name in the notif
+        sessions.broadcast(command.getGameID(), notification, session);
     }
 
     private void handleMakeMove(UserGameCommand command) {
         System.out.println("handling a make move command");
     }
 
-    private void handleLeave(UserGameCommand command, Session session) {
+    private void handleLeave(UserGameCommand command, Session session) throws IOException {
         sessions.removeSessionFromGame(command.getGameID(), session);
         System.out.println("Player left");
+        var notification = new NotificationMessage("Player left."); //TODO also figure out how to send name
+        sessions.broadcast(command.getGameID(), notification, session);
     }
 
     private void handleResign(UserGameCommand command) {
