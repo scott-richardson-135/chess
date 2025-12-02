c2d6047416eaceb82245871f7942fc82eb470593
Scott Richardson <srich1550@gmail.com>
2025-12-01 17:14:31 -0700

    skeleton for handling ws messages to the server

--- shared/src/main/java/websocket/WebSocketHandler.java
+++ shared/src/main/java/websocket/WebSocketHandler.java
@@ -1,10 +1,14 @@
 package websocket;
 
+import com.google.gson.Gson;
 import io.javalin.websocket.*;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
-    private WebSocketSessions sessions;
+    private WebSocketSessions sessions = new WebSocketSessions();
+    private final Gson gson = new Gson();
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -16,10 +20,40 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
     public void handleMessage(WsMessageContext ctx) {
         //determine message type, call connect, makeMove, leaveGame, or resignGame
         System.out.println("received message");
+
+        try {
+            UserGameCommand command = gson.fromJson(ctx.message(), UserGameCommand.class);
+            Session session = ctx.session;
+
+            switch (command.getCommandType()) {
+                case CONNECT -> handleConnectCommand(command);
+                case MAKE_MOVE -> handleMakeMove(command);
+                case LEAVE -> handleLeave(command);
+                case RESIGN -> handleResign(command);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void handleClose(WsCloseContext ctx) {
         System.out.println("Websocket closed");
+    }
+
+    private void handleConnectCommand(UserGameCommand command) {
+        System.out.println("handling a connect command");
+    }
+
+    private void handleMakeMove(UserGameCommand command) {
+        System.out.println("handling a make move command");
+    }
+
+    private void handleLeave(UserGameCommand command) {
+        System.out.println("handling a leave command");
+    }
+
+    private void handleResign(UserGameCommand command) {
+        System.out.println("handling a resign command");
     }
 }
