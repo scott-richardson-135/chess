80a8e6bb3ffa3eeac7589e0cf16bd6fd51283cb0
Scott Richardson <srich1550@gmail.com>
2025-12-04 00:10:45 -0700

    clientside message handlers implemented

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,15 +1,51 @@
 package websocket;
 
-import jakarta.websocket.Endpoint;
-import jakarta.websocket.EndpointConfig;
-import jakarta.websocket.Session;
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+import java.net.URI;
 
 public class WebSocketFacade extends Endpoint {
+    private final GameHandler handler;
+    private Session session;
+    private final Gson gson = new Gson();
 
-    public WebSocketFacade(String url, GameHandler handler) {
 
+    public WebSocketFacade(String url, GameHandler handler) throws Exception {
+        this.handler = handler;
+
+        url = url.replace("http", "ws");
+        URI uri = new URI(url + "/ws");
+
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler((MessageHandler.Whole<String>) this::onMessage);
     }
 
     @Override
     public void onOpen(Session session, EndpointConfig config) {}
+
+    private void onMessage(String jsonMessage) {
+        ServerMessage message = gson.fromJson(jsonMessage, ServerMessage.class);
+
+        switch (message.getServerMessageType()) {
+            case NOTIFICATION -> {
+                NotificationMessage notification = gson.fromJson(jsonMessage, NotificationMessage.class);
+                handler.printMessage(notification.message);
+            }
+            case ERROR -> {
+                ErrorMessage error = gson.fromJson(jsonMessage, ErrorMessage.class);
+                handler.printMessage(error.errorMessage);
+            }
+            case LOAD_GAME -> {
+                LoadGameMessage load = gson.fromJson(jsonMessage, LoadGameMessage.class);
+                handler.updateGame(load.game);
+            }
+        }
+    }
 }
